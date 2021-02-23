53762b92ffb5ef8a76b79e256a5400227959221a
danipereira261 <dpereira261@gmail.com>
2021-02-23 18:08:35 -0300

    aula 5

---
+++ src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,32 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class TestaAlunos {
+
+    public static void main(String[] args) {
+        Collection<String> alunos = new HashSet<>();
+        alunos.add("Rodrigo Turini");
+        alunos.add("Alberto Souza");
+        alunos.add("Nico Steppat");
+        alunos.add("Sergio Lopes");
+        alunos.add("Renan Saggio");
+        alunos.add("Mauricio Aniche");
+
+        boolean pauloEstaMatriculado = alunos.contains("Paulo Silveira");
+        System.out.println(pauloEstaMatriculado);
+
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+
+        List<String> alunosEmLista = new ArrayList<>(alunos);
+
+        System.out.println(alunosEmLista);
+
+
+    }
+}
